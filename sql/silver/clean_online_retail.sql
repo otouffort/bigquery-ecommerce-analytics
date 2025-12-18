1cb9ab5eaa5b20cdb24cef5951909f6e2ed58084
Oto Uffort <otouffort1@gmail.com>
2025-12-18 11:58:59 -0700

    Update clean_online_retail.sql

--- sql/silver/clean_online_retail.sql
+++ sql/silver/clean_online_retail.sql
@@ -1,1 +1,17 @@
+-- Silver layer: cleaned and standardized ecommerce transactions
 
+CREATE OR REPLACE TABLE ecommerce_silver.clean_online_retail AS
+SELECT
+    Invoice,
+    StockCode,
+    Description,
+    Quantity,
+    InvoiceDate,
+    Price,
+    CustomerID,
+    Country,
+    Quantity * Price AS line_revenue
+FROM ecommerce_bronze.raw_online_retail_clean
+WHERE
+    CustomerID IS NOT NULL
+    AND Price > 0;
