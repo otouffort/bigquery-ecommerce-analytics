306a0556e8b0d9c54b33cc519a44e1ed5c2b43b1
Oto Uffort <otouffort1@gmail.com>
2025-12-18 13:36:12 -0700

    Update dim_product.sql

--- sql/gold/dim_product.sql
+++ sql/gold/dim_product.sql
@@ -1,7 +1,23 @@
---Product dimension table with product attributes
+--Product dimension table with product attributes. Product Description assigned based on top frequency. 
 
-CREATE OR REPLACE TABLE ecommerce_gold.dim_product AS
-SELECT DISTINCT
-    StockCode as ProductID,
+CREATE OR REPLACE TABLE ecommerce_gold.dim_products AS
+WITH description_counts AS (
+    SELECT
+        StockCode AS ProductID,
+        Description,
+        COUNT(*) AS cnt
+    FROM ecommerce_silver.clean_online_retail
+    GROUP BY StockCode, Description
+),
+ranked_descriptions AS (
+    SELECT
+        ProductID,
+        Description,
+        ROW_NUMBER() OVER (PARTITION BY ProductID ORDER BY cnt DESC) AS rn
+    FROM description_counts
+)
+SELECT
+    ProductID,
     Description
-FROM ecommerce_silver.clean_online_retail;
+FROM ranked_descriptions
+WHERE rn = 1;
