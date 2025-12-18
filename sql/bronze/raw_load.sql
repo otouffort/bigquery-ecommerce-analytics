bbf8e1dce29be1785cbb179f726f1f963e2ef1e3
Oto Uffort <otouffort1@gmail.com>
2025-12-18 11:11:54 -0700

    Update raw_load.sql

--- sql/bronze/raw_load.sql
+++ sql/bronze/raw_load.sql
@@ -1,1 +1,16 @@
+-- Bronze layer transformation
+-- Standardizes CustomerID from FLOAT to STRING
+-- Handles numeric IDs (e.g. 17850.0) safely
+-- Preserves NULLs and raw data integrity
 
+CREATE OR REPLACE TABLE ecommerce_bronze.raw_online_retail_clean AS
+SELECT
+    Invoice,
+    StockCode,
+    Description,
+    Quantity,
+    InvoiceDate,
+    Price,
+    SAFE_CAST(FLOOR(`Customer ID`) AS STRING) AS CustomerID,
+    Country
+FROM ecommerce_bronze.raw_online_retail;
