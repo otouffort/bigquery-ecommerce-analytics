d3c45cb0436ba35433d0375b4b18f2d0919a3823
Oto Uffort <otouffort1@gmail.com>
2025-12-18 19:50:22 -0700

    Update and rename cohorts.sql to customer_retention.sql

---
+++ analytics/customer_retention.sql
@@ -0,0 +1,16 @@
+--Analysis for: For each group of customers who made their first purchase in a given month (cohort), how many of them were active in subsequent months?
+
+WITH first_purchase AS (
+    SELECT CustomerID, DATE_TRUNC(MIN(InvoiceDate), MONTH) AS cohort_month
+    FROM ecommerce_gold.fact_orders
+    GROUP BY CustomerID
+)
+SELECT
+    fp.cohort_month,
+    DATE_TRUNC(f.InvoiceDate, MONTH) AS order_month,
+    COUNT(DISTINCT f.CustomerID) AS active_customers
+FROM ecommerce_gold.fact_orders f
+JOIN first_purchase fp
+ON f.CustomerID = fp.CustomerID
+GROUP BY cohort_month, order_month
+ORDER BY cohort_month, order_month;
