9236b608fd8d32acf828801259f40afb4db4a683
Oto Uffort <otouffort1@gmail.com>
2025-12-18 19:12:46 -0700

    Create top_products.sql

---
+++ analytics/top_products.sql
@@ -0,0 +1,13 @@
+--Top 10 products by revenue. Result: The top product by lifetime revenue is REGENCY CAKESTAND 3 TIER (Product ID: 22423) with 23,431 units sold for a value of $269,736.70.
+
+SELECT
+    p.ProductID,
+    p.Description,
+    SUM(f.Revenue) AS TotalRevenue,
+    SUM(f.Quantity) AS TotalUnitsSold
+FROM ecommerce_gold.fact_orders f
+JOIN ecommerce_gold.dim_product p
+ON f.ProductID = p.ProductID
+GROUP BY p.ProductID, p.Description
+ORDER BY TotalRevenue DESC
+LIMIT 10;
