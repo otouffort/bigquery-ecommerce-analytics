c5a9d8588882eda7656991b25abf65e894f4a0f6
Oto Uffort <otouffort1@gmail.com>
2025-12-18 18:21:12 -0700

    Update fact_orders.sql

--- sql/gold/fact_orders.sql
+++ sql/gold/fact_orders.sql
@@ -1,1 +1,13 @@
+--Fact table with transactional events (order line events). Includes surrogate key as unique identifier and revenue derived metric.
 
+CREATE OR REPLACE TABLE ecommerce_gold.fact_orders AS
+SELECT
+    GENERATE_UUID() AS OrderLineID,
+    Invoice AS InvoiceID,
+    CustomerID,
+    StockCode AS ProductID,
+    InvoiceDate,
+    Quantity,
+    Price AS UnitPrice,
+    Quantity * Price AS Revenue
+FROM ecommerce_silver.clean_online_retail;
